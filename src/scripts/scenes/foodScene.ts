aa1bf5fb33f9da56e8d4b1cb51dbaa8eab81b7cb
acm13 <annacm@udel.edu>
2020-04-18 15:13:16 -0400

    added images

--- src/scripts/scenes/foodScene.ts
+++ src/scripts/scenes/foodScene.ts
@@ -3,6 +3,20 @@ import ExampleObject from '../objects/exampleObject';
 export default class foodScene extends Phaser.Scene {
     private mapButton: any;
     private background: any;
+    private apple: any;
+    private banana: any;
+    private bread: any;
+    private chips: any;
+    private cookie: any;
+    private hotdog: any;
+    private milk: any;
+    private soda: any;
+    private water: any;
+    private basket: any;
+    private paper: any;
+    private camisaX:any;
+    private dragObj: any;
+
     constructor(){
         super({key: 'foodScene'});
     }
@@ -17,6 +31,75 @@ export default class foodScene extends Phaser.Scene {
         .setInteractive()
         .on('pointerdown', ()=>this.goToMap());
         this.mapButton.setScale(0.6);
+
+        //Add paper and shopping list
+        this.paper=this.add.image(70, 110, "paper");
+        this.paper.setScale(1.2);
+        this.add.text(15,5, "Shopping List:",{fill:"#000000", fontSize:"16px"});
+        this.camisaX = 40;
+        this.add.text(this.camisaX,30, "camisa",{fill:"#000000", fontSize:"16px"});
+        this.add.text(40,50, "zapatos",{fill:"#000000", fontSize:"16px"});
+        this.add.text(40,70, "falda",{fill:"#000000", fontSize:"16px"});
+
+        //Make basket
+        this.basket=this.physics.add.image(270,360,"basket");
+        this.basket.setScale(0.5);
+
+        //Make apple
+        this.apple=this.physics.add.image(200,60,"apple");
+        this.apple.setScale(0.2);
+        this.apple.setInteractive();
+        this.input.setDraggable(this.apple);
+
+        //Make banana
+        this.banana=this.physics.add.image(280,240,"banana");
+        this.banana.setScale(0.3);
+        this.banana.setInteractive();
+        this.input.setDraggable(this.banana);
+
+        //Make bread
+        this.bread=this.physics.add.image(285,60,"bread");
+        this.bread.setScale(0.25);
+        this.bread.setInteractive();
+        this.input.setDraggable(this.bread);
+
+        //Make soda
+        this.soda=this.physics.add.image(195,240,"soda");
+        this.soda.setScale(0.03);
+        this.soda.setInteractive();
+        this.input.setDraggable(this.soda);
+
+        //Make water
+        this.water=this.physics.add.image(280,140,"water");
+        this.water.setScale(0.08);
+        this.water.setInteractive();
+        this.input.setDraggable(this.water);
+
+        //Make hotdog
+        this.hotdog=this.physics.add.image(360,140,"hotdog");
+        this.hotdog.setScale(0.25);
+        this.hotdog.setInteractive();
+        this.input.setDraggable(this.hotdog);
+
+        //Make cookie
+        this.cookie=this.physics.add.image(360,60,"cookie");
+        this.cookie.setScale(0.15);
+        this.cookie.setInteractive();
+        this.input.setDraggable(this.cookie);
+
+        //Make milk
+        this.milk=this.physics.add.image(195,145,"milk");
+        this.milk.setScale(0.25);
+        this.milk.setInteractive();
+        this.input.setDraggable(this.milk);
+
+        //Make chips
+        this.chips=this.physics.add.image(360,240,"chips");
+        this.chips.setScale(0.1);
+        this.chips.setInteractive();
+        this.input.setDraggable(this.chips);
+
+        this.input.on('pointerdown', this.startDrag, this);
     }
 
     update(){
@@ -26,4 +109,22 @@ export default class foodScene extends Phaser.Scene {
     goToMap(){
         this.scene.start('mapScene');
     }
+
+    startDrag(pointer, targets){
+        this.input.off('pointerdown', this.startDrag, this);
+        this.dragObj=targets[0];
+        this.input.on('pointermove', this.doDrag, this);
+        this.input.on('pointerup', this.stopDrag, this);
+    
+      }
+      doDrag(pointer){
+        this.dragObj.x=pointer.x;
+        this.dragObj.y=pointer.y;
+      }
+    
+      stopDrag(){
+        this.input.on('pointerdown', this.startDrag, this);
+        this.input.off('pointermove', this.doDrag, this);
+        this.input.off('pointerup', this.stopDrag, this);
+      }
 }
--- src/scripts/scenes/partyScene.ts
+++ src/scripts/scenes/partyScene.ts
@@ -3,6 +3,18 @@ import ExampleObject from '../objects/exampleObject';
 export default class partyScene extends Phaser.Scene {
     private mapButton: any;
     private background: any;
+    private bowl: any;
+    private firework: any;
+    private greenBalloon: any;
+    private hat: any;
+    private plate: any;
+    private redBalloon: any;
+    private streamers: any;
+    private utensils: any;
+    private paper: any;
+    private camisaX: any;
+    private basket: any;
+    private dragObj: any;
 
     constructor(){
         super({key: 'partyScene'});
@@ -12,12 +24,73 @@ export default class partyScene extends Phaser.Scene {
         this.background=this.add.image(200, 200, "blue");
         this.background.setScale(3.0);
 
-        this.add.text(15, 15, "party Scene", {fill: '#000000', fontSize: '20px'});
+        //Add paper and shopping list
+        this.paper=this.add.image(70, 110, "paper");
+        this.paper.setScale(1.2);
+        this.add.text(15,5, "Shopping List:",{fill:"#000000", fontSize:"16px"});
+        this.camisaX = 40;
+        this.add.text(this.camisaX,30, "camisa",{fill:"#000000", fontSize:"16px"});
+        this.add.text(40,50, "zapatos",{fill:"#000000", fontSize:"16px"});
+        this.add.text(40,70, "falda",{fill:"#000000", fontSize:"16px"});
 
         this.mapButton=this.add.image(70, 300, "mapButton")
         .setInteractive()
         .on('pointerdown', ()=>this.goToMap());
         this.mapButton.setScale(0.6);
+
+        //Make basket
+        this.basket=this.physics.add.image(270,360,"basket");
+        this.basket.setScale(0.5);
+
+        //Make bowl
+        this.bowl=this.physics.add.image(200,60,"bowl");
+        this.bowl.setScale(0.15);
+        this.bowl.setInteractive();
+        this.input.setDraggable(this.bowl);
+
+        //Make firework
+        this.firework=this.physics.add.image(320,240,"firework");
+        this.firework.setScale(0.15);
+        this.firework.setInteractive();
+        this.input.setDraggable(this.firework);
+
+        //Make green balloon
+        this.greenBalloon=this.physics.add.image(285,60,"greenBalloon");
+        this.greenBalloon.setScale(0.1);
+        this.greenBalloon.setInteractive();
+        this.input.setDraggable(this.greenBalloon);
+
+        //Make hat
+        this.hat=this.physics.add.image(230,240,"hat");
+        this.hat.setScale(0.15);
+        this.hat.setInteractive();
+        this.input.setDraggable(this.hat);
+
+        //Make plate
+        this.plate=this.physics.add.image(280,140,"plate");
+        this.plate.setScale(0.15);
+        this.plate.setInteractive();
+        this.input.setDraggable(this.plate);
+
+        //Make red balloon
+        this.redBalloon=this.physics.add.image(360,140,"redBalloon");
+        this.redBalloon.setScale(0.1);
+        this.redBalloon.setInteractive();
+        this.input.setDraggable(this.redBalloon);
+
+        //Make streamers
+        this.streamers=this.physics.add.image(360,60,"streamers");
+        this.streamers.setScale(0.15);
+        this.streamers.setInteractive();
+        this.input.setDraggable(this.streamers);
+
+        //Make shirt
+        this.utensils=this.physics.add.image(195,145,"utensils");
+        this.utensils.setScale(0.15);
+        this.utensils.setInteractive();
+        this.input.setDraggable(this.utensils);
+
+        this.input.on('pointerdown', this.startDrag, this);
     }
 
     update(){
@@ -27,4 +100,22 @@ export default class partyScene extends Phaser.Scene {
     goToMap(){
         this.scene.start('mapScene');
     }
+
+    startDrag(pointer, targets){
+        this.input.off('pointerdown', this.startDrag, this);
+        this.dragObj=targets[0];
+        this.input.on('pointermove', this.doDrag, this);
+        this.input.on('pointerup', this.stopDrag, this);
+    
+      }
+      doDrag(pointer){
+        this.dragObj.x=pointer.x;
+        this.dragObj.y=pointer.y;
+      }
+    
+      stopDrag(){
+        this.input.on('pointerdown', this.startDrag, this);
+        this.input.off('pointermove', this.doDrag, this);
+        this.input.off('pointerup', this.stopDrag, this);
+      }
 }
